45747bc934d887959b57344a6c3ba8805b817266
Chestnut45 <cpt20xx@gmail.com>
2023-11-13 17:34:43 -0400

    rename skybox shaders, separate geometry

---
+++ data/skybox.fs
@@ -0,0 +1,22 @@
+#version 150
+
+uniform samplerCube dayCube;
+uniform samplerCube nightCube;
+
+// Normalized TOD: 0 = noon, 1 = midnight
+uniform float time;
+
+// Direction vector
+in vec3 texCoords;
+
+out vec4 finalColor;
+
+void main()
+{
+    // Sample both skyboxes directly
+    vec4 dayTexel = texture(dayCube, texCoords);
+    vec4 nightTexel = texture(nightCube, texCoords);
+
+    // Blend both texels based on normalized time of day
+    finalColor = mix(dayTexel, nightTexel, time);
+}

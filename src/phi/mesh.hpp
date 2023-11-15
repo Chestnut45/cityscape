fc63c06163c20960d4d81a4dc8d7128dafd04bfb
Chestnut45 <cpt20xx@gmail.com>
2023-11-15 19:46:47 -0400

    update mesh, prepare for HUGE overhaul

--- src/phi/mesh.hpp
+++ src/phi/mesh.hpp
@@ -6,14 +6,15 @@
 
 #include <glm/glm.hpp>
 
+#include "camera.hpp"
 #include "gpubuffer.hpp"
 #include "texture2d.hpp"
 #include "vertex.hpp"
 #include "vertexattributes.hpp"
 #include "shader.hpp"
 
-// Represents a renderable mesh of any internal vertex format
-template <typename Vertex>
+// Represents a renderable mesh with a vertex format and (optionally) an instance data format
+template <typename Vertex, typename InstanceData = glm::mat4>
 class Mesh
 {
     // Interface
@@ -33,7 +34,7 @@ class Mesh
         // Procedural vertex data generation
         void AddSurface(const std::vector<Vertex>& vertices, const std::vector<GLuint>* const indices = nullptr);
         void AddTriangle(const Vertex& a, const Vertex& b, const Vertex& c);
-        void AddQuad();
+        void AddQuad(const Vertex& topLeft, const Vertex& topRight, const Vertex& bottomLeft, const Vertex& bottomRight);
 
         // TODO: Factory mesh generation functions
 
@@ -41,7 +42,9 @@ class Mesh
         void Commit();
 
         // Rendering methods
-        void Draw(const Shader& shader);
+        void Draw(const Shader& shader, const Camera& camera, const glm::mat4& transform);
+        void DrawInstance(const InstanceData& instanceData);
+        void FlushInstances(const Shader& shader, const Camera& camera);
 
         // Clear all mesh data, cleanup resources, return to initial state
         void Reset();

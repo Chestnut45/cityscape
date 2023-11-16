70a32d6c72b8e370d8448e9b654156049d209e1b
Chestnut45 <cpt20xx@gmail.com>
2023-11-16 18:36:48 -0400

    start add app class to phi

---
+++ src/phi/app.cpp
@@ -0,0 +1,9 @@
+#include "app.hpp"
+
+namespace Phi
+{
+    App::App(const std::string& name, int glMajVer, int glMinVer) : name(name)
+    {
+        
+    }
+}
---
+++ src/phi/app.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <glm/glm.hpp>
+
+namespace Phi
+{
+    // Main app class, handles OpenGL context creation and input
+    class App
+    {
+        public:
+
+            App(const std::string& name, int glMajVer, int glMinVer);
+            virtual ~App();
+
+            virtual void Run();
+            virtual void Update(float delta) = delete;
+            virtual void Render() = delete;
+
+            // Accessors
+            glm::vec2 GetScreenSize() const;
+            GLFWwindow* GetWindow() const { return m_pWindow; }
+
+            // Used internally but needs to be public
+            void _setMouseScroll(const glm::vec2 &scroll);
+
+        protected:
+
+            // App details
+            std::string name;
+            int glMajorVersion;
+            int glMinorVersion;
+
+            // Window size
+            int wWidth = 0;
+            int wHeight = 0;
+
+            // Input helpers
+            bool IsKeyDown(int key) const;
+            bool IsKeyJustDown(int key) const;
+            bool IsLMBDown() const;
+            bool IsRMBDown() const;
+            bool IsMMBDown() const;
+            glm::vec2 GetMousePos() const;
+            glm::vec2 GetMouseScroll() const { return m_mouseScroll; }
+
+        private:
+
+            // Helpers
+            void Init();
+    };
+}

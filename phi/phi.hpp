557fe009033b27017c0d8d34a9eade44404182ce
Chestnut45 <cpt20xx@gmail.com>
2023-11-18 18:04:08 -0400

    add single include header for phi

---
+++ phi/phi.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// Single include to access entire Phi engine
+
+#include "app.hpp"
+#include "camera.hpp"
+#include "cubemap.hpp"
+#include "framebuffer.hpp"
+#include "geometry.hpp"
+#include "gpubuffer.hpp"
+#include "mesh.hpp"
+#include "shader.hpp"
+#include "texture2d.hpp"
+#include "vertex.hpp"
+#include "vertexattributes.hpp"

c2c51341eb82a0d88a2a2dfede8aea144e6a8ff5
Chestnut45 <cpt20xx@gmail.com>
2023-11-22 16:20:09 -0400

    model class

---
+++ phi/model.cpp
@@ -0,0 +1,46 @@
+#include "model.hpp"
+
+namespace Phi
+{
+    Model::Model(const std::string& objPath)
+    {
+        // std::vector<Vertex> vertices;
+
+        // // Copy all vertices from the mesh
+        // for (size_t i = 0; i < mesh.mNumVertices; ++i)
+        // {
+        //     Vertex v;
+
+        //     v.x = mesh.mVertices[i].x;
+        //     v.y = mesh.mVertices[i].y;
+        //     v.z = mesh.mVertices[i].z;
+
+        //     v.nx = mesh.mNormals[i].x;
+        //     v.ny = mesh.mNormals[i].y;
+        //     v.nz = mesh.mNormals[i].z;
+
+        //     // Add colors if present
+        //     if (mesh.mColors[0])
+        //     {
+        //         v.r = mesh.mColors[0][i].r;
+        //         v.g = mesh.mColors[0][i].g;
+        //         v.b = mesh.mColors[0][i].b;
+        //         v.a = mesh.mColors[0][i].a;
+        //     }
+
+        //     // Add UVs if present
+        //     if (mesh.mTextureCoords[0])
+        //     {
+        //         v.u1 = mesh.mTextureCoords[0][i].x;
+        //         v.v1 = mesh.mTextureCoords[0][i].y;
+        //     }
+        //     if (mesh.mTextureCoords[1])
+        //     {
+        //         v.u2 = mesh.mTextureCoords[1][i].x;
+        //         v.v2 = mesh.mTextureCoords[1][i].y;
+        //     }
+            
+        //     vertices.push_back(v);
+        // }
+    }
+}
---
+++ phi/model.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <glm/glm.hpp>
+
+#include "mesh.hpp"
+
+namespace Phi
+{
+    class Model
+    {
+        // Interface
+        public:
+            
+            // Internal vertex format for models
+            typedef VertexPosColorNormUv1Uv2 Vertex;
+
+            Model(const std::string& objPath);
+            ~Model();
+
+            // Delete copy constructor/assignment
+            Model(const Model&) = delete;
+            Model& operator=(const Model&) = delete;
+
+            // Delete move constructor/assignment
+            Model(Model&& other) = delete;
+            void operator=(Model&& other) = delete;
+
+            // Immediately render to the current FBO
+            void Draw(const Shader& shader);
+
+            // Immediately render iData.size() instances of the model to the current FBO, uploads iData 
+            // directly to the static instance buffer and binds it to SSBOBinding::InstanceBuffer
+            template <typename InstanceData>
+            void DrawInstances(const Shader& shader, const std::vector<InstanceData>& iData);
+
+        // Data / implementation
+        private:
+            
+            std::vector<Mesh<Vertex>> meshes;
+
+            // Static resources
+            
+            // Instance buffer used by all models
+            static inline GPUBuffer* instanceBuffer = nullptr;
+    };
+}
